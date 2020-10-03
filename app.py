177875fa561d79d0268eb234255742ee6fda0ddb
nemounas <skochew@yandex.ru>
2020-10-03 22:33:35 +0300

    migration-mysql tag

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from config import Configuration
-
 from flask_sqlalchemy import SQLAlchemy
+
+# Migrate database
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
+
+# Migrate database
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
