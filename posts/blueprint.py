19a4e7e4dd6909e76e7b2e8ca19b0252fd534f0d
nemounas <skochew@yandex.ru>
2020-10-07 16:44:51 +0300

    404

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -16,7 +16,7 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 @posts.route('/<slug>/edit/', methods=['POST', 'GET'])
 @login_required
 def edit_post(slug):
-    post = Post.query.filter(Post.slug == slug).first()
+    post = Post.query.filter(Post.slug == slug).first_or_404()
 
     if request.method == 'POST':
         form = PostForm(formdata=request.form, obj=post)
@@ -71,13 +71,13 @@ def index():
 
 @posts.route('/<slug>')
 def post_detail(slug):
-    post = Post.query.filter(Post.slug == slug).first()
+    post = Post.query.filter(Post.slug == slug).first_or_404()
     tags = post.tags
     return render_template('posts/post_detail.html', post=post, tags=tags)
 
 
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
-    tag = Tag.query.filter(Tag.slug == slug).first()
+    tag = Tag.query.filter(Tag.slug == slug).first_or_404()
     posts = tag.posts.all()
     return render_template('posts/tag_detail.html', tag=tag, posts=posts)
--- view.py
+++ view.py
@@ -4,5 +4,10 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    name = 'Ivan'
+    name = 'Guest'
     return render_template('index.html', n=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
